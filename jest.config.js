725d784439a514e25839baad9cd084db361dda65
dudiq <idudiq@gmail.com>
2019-02-26 09:07:26 +0300

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const { getEnv } = require('./env');
+
+const { NODE_ENV } = process.env;
+const ENV = getEnv(NODE_ENV);
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['node_modules'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['./**/*.spec.(js|jsx|ts|tsx)'],
+  setupFiles: ['<rootDir>/tests/unit/setup'],
+  globals: {
+    ENV,
+  },
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+one-pixel 
+=
+2D canvas
+
+many thx to [http://phrogz.net/tmp/canvas_zoom_to_cursor.html](http://phrogz.net/tmp/canvas_zoom_to_cursor.html)
+
+(c) 2019. dudiq
